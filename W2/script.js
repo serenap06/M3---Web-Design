32ac0a6492655db89346af908518814bba583410
serenap06 <168187800+serenap06@users.noreply.github.com>
2026-06-10 14:02:58 +0200

    aggiunta bottoni

--- W2/script.js
+++ W2/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper',{
+const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     navigation: {
@@ -9,4 +9,36 @@ const swiper = new Swiper('.swiper',{
     autoplay: {
         delay: 5000,
     }
+})
+
+const button = document.querySelector(".custom-button")
+const albumsCollapse = document.querySelector(".collapsible-albums")
+const tracksBtn = document.querySelector(".tracks-btn")
+const tracksList = document.querySelector(".custom-list")
+
+function showContent(element) {
+    const isHidden = element.classList.contains("d-none")
+    if (isHidden) {
+        element.classList.remove("d-none")
+    } else {
+        element.classList.add("d-none")
+    }
+}
+button.addEventListener("click", hideCollapse)
+
+function changeButtonLable(element, btn) {
+    if (!element.classList.contains("d-none")) {
+        btn.innerText = "Hide content"
+    } else {
+        btn.innerText = "Show content"
+    }
+}
+
+function hideCollapse() {
+    showContent(albumsCollapse)
+    changeButtonLable(albumsCollapse, button)
+}
+
+tracksBtn.addEventListener("click", () => {
+    showContent(tracksList)
 })
